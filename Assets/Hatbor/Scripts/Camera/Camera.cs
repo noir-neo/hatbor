cbedf9a697e8dc1e47067a4537789536497472c2
Shoma SATO <noir.neo.04@gmail.com>
2023-11-06 03:15:09 +0900

    Workaround for transparent background

--- Assets/Hatbor/Scripts/Camera/Camera.cs
+++ Assets/Hatbor/Scripts/Camera/Camera.cs
@@ -1,6 +1,8 @@
 using System;
+using Hatbor.Config;
 using Hatbor.Rig;
 using UniRx;
+using UnityEngine.Rendering.Universal;
 using VContainer;
 using VContainer.Unity;
 
@@ -9,19 +11,24 @@ namespace Hatbor.Camera
     public sealed class Camera : IStartable, ITickable, IDisposable
     {
         readonly UnityEngine.Camera mainCamera;
+        readonly UniversalAdditionalCameraData cameraData;
         readonly ICameraRig cameraRig;
         readonly RenderTextureProvider renderTextureProvider;
+        readonly RenderConfig renderConfig;
 
         readonly CompositeDisposable disposables = new();
 
         [Inject]
         public Camera(UnityEngine.Camera mainCamera,
             ICameraRig cameraRig,
-            RenderTextureProvider renderTextureProvider)
+            RenderTextureProvider renderTextureProvider,
+            RenderConfig renderConfig)
         {
             this.mainCamera = mainCamera;
+            this.cameraData = mainCamera.GetComponent<UniversalAdditionalCameraData>();
             this.cameraRig = cameraRig;
             this.renderTextureProvider = renderTextureProvider;
+            this.renderConfig = renderConfig;
         }
 
         void IStartable.Start()
@@ -30,6 +37,11 @@ namespace Hatbor.Camera
 
             renderTextureProvider.OnSizeChanged
                 .Subscribe(_ => mainCamera.ResetAspect())
+                .AddTo(disposables);
+
+            // TODO: support transparent with post-processing
+            renderConfig.TransparentBackground
+                .Subscribe(b => cameraData.renderPostProcessing = !b)
                 .AddTo(disposables);
         }
 
