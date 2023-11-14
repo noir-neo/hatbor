485e51f120ba9551c7547727db1688cb5b3c0813
Shoma SATO <noir.neo.04@gmail.com>
2023-11-15 03:02:05 +0900

    Workaround: Apply received eye direction

--- Assets/Hatbor/Scripts/Rig/AvatarRig.cs
+++ Assets/Hatbor/Scripts/Rig/AvatarRig.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using UniVRM10;
 using VContainer;
 
@@ -8,6 +9,9 @@ namespace Hatbor.Rig
         readonly IRootTransformRig rootTransformRig;
         readonly IHumanoidRig humanoidRig;
         readonly IExpressionRig expressionRig;
+
+        static readonly FieldInfo EyeDirectionApplicableFieldInfo = typeof(Vrm10RuntimeExpression)
+            .GetField("_eyeDirectionApplicable", BindingFlags.NonPublic | BindingFlags.Instance);
 
         [Inject]
         public AvatarRig(IRootTransformRig rootTransformRig,
@@ -24,6 +28,11 @@ namespace Hatbor.Rig
             // NOTE: Runtime の生成より前に instance.transform を動かすと ControlRig がズレる (UniVRM v0.115.0)
             // ので、とりあえず初期化しておく必要あり
             var runtime = instance.Runtime;
+
+            // NOTE: LookAt が eye bone や expression を上書きするので粉砕する
+            // TODO: VMC Protocol 送信元の VRM と異なる場合に curve map の適用を再計算すべきだが、元の視線情報も必要になる
+            // runtime.Expression._eyeDirectionApplicable = null;
+            EyeDirectionApplicableFieldInfo.SetValue(runtime.Expression, null);
         }
 
         public void Update(Vrm10Instance instance)
